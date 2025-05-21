0c7384a4438d150f01fb8380d632150dbab21d45
PierrunoYT <pierrebruno@hotmail.ch>
2025-05-21 18:49:23 +0200

    Add create_folder tool and register it in config and tool registry

--- ai_cli/tools/__init__.py
+++ ai_cli/tools/__init__.py
@@ -8,6 +8,8 @@ from ai_cli.tools.rename import RenameTool
 from ai_cli.tools.remove import RemoveFileTool
 from ai_cli.tools.create_file import CreateFileTool
 from ai_cli.tools.copy_file import CopyFileTool
+from ai_cli.tools.move_file import MoveFileTool
+from ai_cli.tools.create_folder import CreateFolderTool
 
 # Register all available tools
 AVAILABLE_TOOLS = {
@@ -16,4 +18,6 @@ AVAILABLE_TOOLS = {
     "remove": RemoveFileTool,
     "create_file": CreateFileTool,
     "copy_file": CopyFileTool,
+    "move_file": MoveFileTool,
+    "create_folder": CreateFolderTool,
 }
